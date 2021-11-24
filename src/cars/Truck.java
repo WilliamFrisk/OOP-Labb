7417f350b63848c1e51de97ef819c1ce2a46e976
havreman <93663377+havreman@users.noreply.github.com>
2021-11-24 14:54:07 +0100

    test

---
+++ src/cars/Truck.java
@@ -0,0 +1,14 @@
+package cars;
+
+import java.awt.*;
+
+public abstract class Truck extends Vehicle {
+    public Truck (int nrDoors, double enginePower, Color color, String modelName){
+        super(nrDoors, enginePower, color, modelName);
+    }
+
+
+// ej sandra
+
+
+}
