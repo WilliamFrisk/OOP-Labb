8facabea23aa8e45a4fae929b2ab872372fa1e78
William <william.hp.frisk@gmail.com>
2021-11-24 14:42:09 +0100

    Ny inheritance hierarki

---
+++ src/cars/Scania.java
@@ -0,0 +1,15 @@
+package cars;
+
+import java.awt.*;
+
+public class Scania extends Truck {
+
+    public Scania() {
+        super(2, 200, Color.white, "Scania");
+    }
+
+    @Override
+    double speedFactor() {
+        return 0;
+    }
+}
