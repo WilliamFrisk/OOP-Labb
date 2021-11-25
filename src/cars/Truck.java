e14a526c7baeddb7b9352718484394815ad6ec8d
havreman <93663377+havreman@users.noreply.github.com>
2021-11-25 15:08:37 +0100

    implementerade tipp_funk

--- src/cars/Truck.java
+++ src/cars/Truck.java
@@ -7,7 +7,32 @@ public abstract class Truck extends Vehicle {
         super(nrDoors, enginePower, color, modelName);
     }
 
-//hej sandra
+    private int platformAngle;
+    private double weight;
 
+    public int getAnglePlatform() {
+        return platformAngle;
+    }
+
+    // Metod för att tippa flaket
+    private void raisePlatform() {
+        if (platformAngle == 70) {
+            System.out.println("Platform at highest point");
+        } else {
+            platformAngle ++;
+        }
+    }
+
+    // Metod för att sänka tillbaka flaket
+    private void lowerPlatform() {
+        if (platformAngle == 0) {
+            System.out.println("Platform at lowest point");
+        } else {
+            platformAngle --;
+        }
+    }
+
+// Ska det vara hålla inne och sänka eller ska den sänkas till 0 direkt?
+// Samma sak på höjningen, olika nivåer eller hålla inne till ev maxhöjd?
 
 }
